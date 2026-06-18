3e2f82fa76149a9f87a194e16c5aa7cd982bcc42
sharmachirag5898-sys <sharmachirag0720@gmail.com>
2026-06-18 12:46:22 +0530

    Practice_Code_of_18th_June_2026

---
+++ DutchNationalFlagAlgorithm.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main(){
+
+    int nums[] = {2, 0, 1, 2, 1, 0, 0, 1};
+    int size = sizeof(nums)/sizeof(nums[0]);
+
+    int count0 = 0;
+    int count1 = 0;
+    int count2 = 0;
+
+    for(int i = 0; i < size; i++){
+        if(nums[i]==0){
+            count0++;
+        }
+        else if(nums[i]==1){
+            count1++;
+        }
+        else if(nums[i]==2){
+            count2++;
+        }
+    }
+
+    for(int i = 0; i < size; i++){
+
+        if(i<count0){
+            nums[i]=0;
+        }
+        else if (i<(count0 + count1)){
+            nums[i]=1;
+        }
+        else{
+            nums[i]=2;
+        }
+    }
+
+    printf("Sorted array:");
+    for(int i = 0; i < size; i++){
+        printf("%d", nums[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
